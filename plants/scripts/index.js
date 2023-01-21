af899a81508800fa7bfd26f7590ee547dd95651f
Iryna Kanavalchuk <irkinbk@gmail.com>
2023-01-21 14:01:28 +0300

    add section scrolling with JS

--- plants/scripts/index.js
+++ plants/scripts/index.js
@@ -1,4 +1,4 @@
-console.log(`
+/*console.log(`
 Привет, надеюсь проверка моей работы не отнимет много времени.))\n
 Рассчитываю на максимальный балл, все требования выполнены.\n
 Всегда благодарна за дельные замечания и советы. Готова ответить на любые вопросы по работе.\n
@@ -17,4 +17,27 @@ console.log(`
 - интерактивность реализована либо по макету, либо добавлено самостоятельно, плавность реализована через transition, интерактивность не влияет на соседние элементы. \n
 - интерактивность окон секций Price и Contact us не реализовывалась, т.к. это задание следующих частей. Повороты стрелок и смена цвета сделаны для примера.\n
 
-Спасибо за потраченное на мою работу время!!!!`);
+Спасибо за потраченное на мою работу время!!!!`);*/
+
+
+//Прокрутка при клике
+const navigationLinks = document.querySelectorAll('.navigation__link>*[data-goto]');
+if (navigationLinks.length > 0) {
+   navigationLinks.forEach(navigationLink => {
+      navigationLink.addEventListener("click", onNavigationLinkClick);
+   });
+
+   function onNavigationLinkClick(e) {
+      const navigationLink = e.target;
+      if (navigationLink.dataset.goto && document.querySelector(navigationLink.dataset.goto)) {
+         const gotoSection = document.querySelector(navigationLink.dataset.goto);
+         const gotoSectionValue = gotoSection.getBoundingClientRect().top + scrollY;
+
+         window.scrollTo({
+            top: gotoSectionValue,
+            behavior: "smooth"
+         });
+         e.preventDefault();
+      }
+   }
+}
