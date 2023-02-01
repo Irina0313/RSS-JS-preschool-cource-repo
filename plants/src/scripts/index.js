c31e99419939b861b5551979b709e07649764244
Iryna Kanavalchuk <irkinbk@gmail.com>
2023-02-01 19:26:02 +0300

    feat: add interactive behaviour service section

--- plants/src/scripts/index.js
+++ plants/src/scripts/index.js
@@ -1,4 +1,4 @@
-console.log(`
+/*console.log(`
 Привет, надеюсь проверка моей работы не отнимет много времени.))\n
 Рассчитываю на максимальный балл, все требования выполнены.\n
 Всегда благодарна за дельные замечания и советы. Готова ответить на любые вопросы по работе.\n
@@ -33,7 +33,7 @@ console.log(`
 21) ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям (все, кроме Account, она пока просто закрывает меню) \n
 22) при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, также скрытие меню происходит если сделать клик вне данного окна \n
 
-Спасибо за потраченное на мою работу время!!!!`);
+Спасибо за потраченное на мою работу время!!!!`);*/
 
 
 
@@ -49,7 +49,6 @@ if (burger) {
       document.body.classList.toggle('_lock');
       burger.classList.toggle('_active');
       headerNavigation.classList.toggle('_active');
-      console.log('33');
 
       if (burger.classList.contains('_active')) {
          if (sections.length > 0) {
@@ -64,8 +63,7 @@ if (burger) {
             )
          }
       }
-   }
-   )
+   })
 }
 //Прокрутка при клике
 const navigationLinks = document.querySelectorAll('.navigation__link>*[data-goto]');
@@ -94,4 +92,125 @@ if (navigationLinks.length > 0) {
       }
    }
 }
+
+window.onload = function () {
+
+   //Блюр
+   addButtonsClickHandler();
+}
+
+//hover секция Service
+
+const button = document.querySelectorAll('.service-button');
+button.forEach(btn => {
+   btn.addEventListener('mouseover', () => {
+      btn.classList.add('focused');
+   })
+   btn.addEventListener('mouseout', () => {
+      btn.classList.remove('focused');
+   })
+   btn.addEventListener('click', () => {
+      btn.classList.remove('focused');
+
+   })
+})
+
+const addButtonsClickHandler = () => {
+
+   document.querySelector('.service__buttons').addEventListener('click', (e) => {
+
+      if (e.target.classList.contains('service-button')) {
+         let clickedButton = e.target;
+
+         selectClickedButton(clickedButton);
+         filterServiceItems(clickedButton);
+      }
+   })
+}
+
+const selectClickedButton = (clickedButton) => {
+   clickedButton.classList.toggle('_clicked');
+   clickedButton.classList.toggle('button_unactive');
+   checkClickedButtons();
+}
+
+const checkClickedButtons = () => {
+   let buttons = document.querySelectorAll('._clicked');
+   if (buttons.length > 1) {
+      blockUnclickedButton();
+   } else {
+      unblockButton();
+   }
+}
+
+const blockUnclickedButton = () => {
+   let btn = document.querySelectorAll('.service-button');
+   btn.forEach(btn => {
+      if (btn.classList.contains('button_unactive'
+      )) {
+         btn.setAttribute('disabled', '');
+         btn.classList.add('_blocked');
+      }
+   })
+}
+const unblockButton = () => {
+   let btn = document.querySelectorAll('.service-button');
+   btn.forEach(btn => {
+      if (btn.classList.contains('button_unactive'
+      )) {
+         btn.removeAttribute('disabled', '');
+         btn.classList.remove('_blocked')
+      }
+   })
+}
+
+const filterServiceItems = (clickedButton) => {
+   let serveceItems = document.querySelectorAll('.service-item');
+
+   serveceItems.forEach(item => {
+      if (clickedButton.classList.contains('_clicked')) {
+         if (item.classList.contains(clickedButton.innerText)) {
+            item.classList.remove('_blur')
+            item.classList.add('_active')
+         } else if ((!item.classList.contains(clickedButton.innerText)) && (!item.classList.contains('_active'))) {
+            item.classList.add('_blur')
+         }
+      } else if (clickedButton.classList.contains('button_unactive')) {
+         if (item.classList.contains(clickedButton.innerText)) {
+            item.classList.remove('_active')
+            item.classList.add('_blur')
+         }
+      }
+      checkUnclickedButtons();
+   })
+
+}
+const checkUnclickedButtons = () => {
+   let btn = document.querySelectorAll('.service-button');
+   let result = 0;
+   btn.forEach(btn => {
+
+      if (btn.classList.contains('_clicked')) {
+         result = result + 1;
+      } else {
+         result = result + 0;
+      }
+   })
+   console.log(result)
+   if (result === 0) {
+      removeBlure();
+   }
+}
+const removeBlure = () => {
+   let serveceItems = document.querySelectorAll('.service-item');
+   serveceItems.forEach(item => {
+      item.classList.remove('_blur')
+   })
+}
+
+
+
+
+
+
 
